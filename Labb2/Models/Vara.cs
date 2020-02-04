cd86c2acf2dca2eef3aba9ec7a0851a1ff3b831b
Linus Björk <libj0015@student.hv.se>
2020-02-04 11:33:30 +0100

    Lagt till bootstrap i webbapplikation och lagt in modeller

---
+++ Labb2/Models/Vara.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Labb2.Models
+{
+    public class Vara
+    {
+        public int ID { get; set; }
+
+        public string Namn { get; set; }
+
+        public double Pris { get; set; }
+
+        public int ButikAntal { get; set; }
+
+        public int LagerAntal { get; set; }
+    }
+}
